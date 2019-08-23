6890818c860d0d921fb9a21426366fa1fecf1111
Alex <Alexander.maehl@grappler.io>
2019-08-23 14:02:34 +1200

    added user details to home screen

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import {app} from "./Firebase";
+import * as firebase from "firebase/app";
 
 const Home = () => {
   return (
     <React.Fragment>
       <h1>Home</h1>
-      <button onClick={() => app.auth().signOut()}>Sign out</button>
+      <h2>Welcome {firebase.auth().currentUser.displayName}</h2>
+      <img alt="profile picture" src={firebase.auth().currentUser.photoURL}/>
+      <h3><button onClick={() => app.auth().signOut()}>Sign out</button></h3>
     </React.Fragment>
   );
 };
