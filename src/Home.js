343756bf0c087bcbd872411a1e46fde1bcd183f6
Alex <Alexander.maehl@grappler.io>
2019-08-23 15:48:33 +1200

    removed unessisary firebase import

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import {app} from "./Firebase";
-import * as firebase from "firebase/app";
+
+
 
 const Home = () => {
   return (
     <React.Fragment>
       <h1>Home</h1>
-      <h2>Welcome {firebase.auth().currentUser.displayName}</h2>
-      <img alt="profile picture" src={firebase.auth().currentUser.photoURL}/>
+      <h2>{app.auth().currentUser.displayName}</h2>
+      <img alt="profile" src={app.auth().currentUser.photoURL}/>
       <h3><button onClick={() => app.auth().signOut()}>Sign out</button></h3>
     </React.Fragment>
   );
