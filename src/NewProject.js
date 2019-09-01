992d6849891bb4daa8a7ed86c2e647cedce3b580
12452williamning@gmail.com <12452williamning@gmail.com>
2019-09-01 21:50:21 +1200

    Layout neater and drag and drop displays image with some issues

--- src/NewProject.js
+++ src/NewProject.js
@@ -1,12 +1,31 @@
 import React from 'react';  
 import { app } from "./Firebase";
 import "./index.css";
-import { Button } from 'react-bootstrap';
+import { Button, Icon, AutoComplete } from "antd";
+import DragAndDrop from "./DragAndDrop.js"
 
 class NewProjectPopup extends React.Component {
     constructor(props) {
         super(props)
         
+    }
+    //@@TODO, fix the mix of inline and not inline css. v.dirty
+
+    state = {
+        projectTitle : "",
+        projectTopic : "",
+        Image : "",
+    }
+    handleDrop = (file) => {        
+        var reader  = new FileReader();
+        reader.onload = function(e)  {
+            var image = document.createElement("img");
+            image.src = e.target.result;
+            var imagePane = document.getElementById("imagePane");
+            imagePane.innerHTML = "";
+            imagePane.appendChild(image);
+         }
+         reader.readAsDataURL(file[0]);
     }
 
     render() {
@@ -15,18 +34,47 @@ class NewProjectPopup extends React.Component {
             <React.Fragment>
                 <div className='popup'>
                     <div className='inner'>
-                        <h1 
-                        style = {{
-                            textAlign: "center",
-                            marginTop: "0.5em"
-                        }} 
-                        >NewProject</h1>  
-
-                        <Button
-                        onClick={() => 
-                            togglePopup()
-                          }
-                        >???</Button>
+                        <Icon 
+                            style = {{
+                                position:"absolute",
+                                right: "15px",
+                                top: "15px"
+                            }}
+                            type="close" 
+                            onClick={() => 
+                                togglePopup()
+                            }
+                        />        
+                        <h1>
+                            New Project                                
+                        </h1>
+                        <div
+                            style ={{
+                                marginLeft: "35%",
+                                marginRight: "35%"
+                            }}
+                            >                    
+                                <form>
+                                    <div>Project Title</div>
+                                    <input type="text" className="textInput" title="name" />
+                                    <div>Project Topic</div>
+                                    <input type="text" className="textInput" topic="name" />
+                                </form>
+                        </div>
+                        <div 
+                            style = {{
+                                marginLeft: "25%",
+                                marginRight: "25%",
+                                textAlign:"center"
+                            }}>
+                            <DragAndDrop handleDrop={this.handleDrop}>
+                                <div id="imagePane">
+                                    Drag Image here
+                                </div>
+                            </DragAndDrop>
+                            <br/>
+                            <Button>Create</Button>
+                        </div>
                     </div>
                 </div>
 
