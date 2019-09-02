5c5e24082b2b306ce44b554ea6ec12175d401c82
Huizhou Zhan <hyacinthzhz@hotmail.com>
2019-09-02 20:55:11 +1200

    DLO-18 add more operating project collections function

--- src/Firestore.js
+++ src/Firestore.js
@@ -1,9 +1,8 @@
 import { app } from "./Firebase";
-import firestore from "firebase/firestore";
-import React from 'react';
 
 var db = app.firestore();
 class Firestore {
+
     static saveWithDocID(collection, docID, data) {
         db.collection(collection).doc(docID).set(data, { merge: true })
             .then(function() {
@@ -15,9 +14,11 @@ class Firestore {
                 console.error("Error writing document: ", error);
             })
     };
+
     static getCollection(collection) {
         return db.collection(collection).get();
     };
+
     static getDocData(collection, docID) {
         var docRef = db.collection(collection).doc(docID);
         docRef.get().then(function (doc) {
@@ -29,7 +30,54 @@ class Firestore {
         }).catch(function (error) {
             console.log("Error getting document:", error);
         });
-    }
+    };
+
+    static toArray(datas) {
+        var array = [];
+        datas.get().then(function (querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                array[doc.docID] = doc.data();
+            });
+        });
+        return array;
+    };
+
+    static getAllProjectsByUser(userID) {
+        return db.collection("users").doc(userID).collection("projects"); 
+    };
+
+    static getProjectById(userID, projectID) {
+        return this.getAllProjectsByUser(userID).doc(projectID);
+    };
+
+    static getAllIdeasByProject(userID, projectID) {
+        return this.getProjectById(userID, projectID).collection("Ideas");
+    };
+
+    static saveToDBWithDocID(collection, docID, data) {
+        collection.doc(docID).set(data, {merge : true})
+        .then(function () {
+            collection.doc(docID).update({
+                timestamp: Math.floor(Date.now())
+            });
+        }).catch(function (error) {
+            console.error("Error writing document: ", error);
+        })
+    };
+
+    static saveIdeasToProject(userID, projectID, ideas) {
+        var ideaCollection = this.getAllIdeasByProject(userID, projectID);
+        ideas.forEach(function(idea) {
+            this.saveToDBWithDocID(ideaCollection, idea.id, idea);
+        });
+    };
+
+    static saveProjectsToUser(userID, projects) {
+        var projectCollection = this.getAllProjectsByUser(userID);
+        projects.forEach(function (project) {
+            this.saveToDBWithDocID(projectCollection, project.id, project);
+        });
+    };
 };
 
 export default Firestore;
