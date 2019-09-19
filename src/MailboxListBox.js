e0cb9a107000812299e32adf69ee7fc15b405ccc
Huizhou Zhan <hyacinthzhz@hotmail.com>
2019-09-19 16:52:17 +1200

    change pagination to react-bootstrap pagination, however the first&prev&next&last button sometimes not work

--- src/MailboxListBox.js
+++ src/MailboxListBox.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import Pagination from 'react-js-pagination';
+// import Pagination from 'react-js-pagination';
 import MailboxList from "./MailboxList";
 import Firestore from "./Firestore";
 import firebase from "firebase";
+import Pagination from "react-bootstrap/Pagination";
 // import Pagination from 'react-bootstrap/Pagination';
 
 class MailboxListBox extends React.Component {
@@ -13,8 +14,14 @@ class MailboxListBox extends React.Component {
         this.state = {
             data: [],
             offset: 0,
-            pageSize: this.props.pageSize
+            active: 1,
+            pageSize: this.props.pageSize,
+            showFirst: true,
+            showPrev: true,
+            showNext: true,
+            showLast: true
         };
+        this.handlePageChange = this.handlePageChange.bind(this);
     }
 
     getShareList() {
@@ -27,6 +34,7 @@ class MailboxListBox extends React.Component {
                 listMessages.push(proj);
             });
             this.setState({ data: listMessages, pageCount: Math.ceil(listMessages.length / this.state.pageSize) });
+            // this.toggleFirstPrevNextLast();
         });
 
     }
@@ -35,24 +43,70 @@ class MailboxListBox extends React.Component {
         this.getShareList();
     }
 
-    handlePageChange = (pageNumber) => {
-        this.setState({ offset: pageNumber - 1 });
+    // toggleFirstPrevNextLast() {
+    //     let offset = this.state.offset;
+    //     if (offset == 0 || this.state.pageCount < 1) {
+    //         this.setState({ showPrev: false, showFirst: false });
+    //     } else if (this.state.pageCount < 1 || offset == this.state.pageCount - 1) {
+    //         this.setState({ showNext: false, showLast: false });
+    //     } else {
+    //         this.setState({ showPrev: true, showFirst: true, showNext: true, showLast: true });
+    //     }
+    // }
+
+    handlePageChange(e) {
+        console.log(e.target.text);
+        let offset = this.state.offset;
+        if (offset > 0 && "‹Previous" === e.target.text) {
+            offset = offset - 1;
+        } else if ("«First" === e.target.text) {
+            offset = 0;
+        } else if ("»Last" === e.target.text) {
+            offset = this.state.pageCount - 1;
+        } else if (offset < this.state.pageCount - 1 && "›Next" === e.target.text) {
+            offset = offset + 1;
+        } else {
+            offset = parseInt(e.target.text) - 1;
+        }
+        this.setState({ offset: offset, active: offset + 1 });
+        this.generatePageItems();
     };
+
+    generatePageItems() {
+        let items = [];
+        if (this.state.pageCount <= 10) {
+            for (let i = 1; i <= this.state.pageCount; i++) {
+                items.push(<Pagination.Item key={i} active={i === this.props.active}>{i}</Pagination.Item>)
+            }
+        } else {
+            items = [];
+            for (let i = this.state.active; i <= this.state.pageCount; i++) {
+                items.push(<Pagination.Item key={i} active={i === this.props.active}>{i}</Pagination.Item>)
+            }
+            let leftpad = items.length;
+            if (leftpad < 10 && this.state.active > leftpad) {
+                let tmpitems = items;
+                items = [];
+                for (let i = this.state.active - leftpad; i < this.state.active; i++) {
+                    items.push(<Pagination.Item key={i} active={i === this.props.active}>{i}</Pagination.Item>);
+                }
+                items.push(tmpitems);
+            }
+        }
+        return items;
+    }
 
     render() {
         return (
             <div className="messageBox">
                 <MailboxList data={this.state.data.slice(this.state.offset * this.state.pageSize, (this.state.offset + 1) * this.state.pageSize)} />
-                <div>
-                    <Pagination
-                        hideDisabled
-                        activePage={this.state.offset}
-                        itemsCountPerPage={this.state.pageSize}
-                        totalItemsCount={this.state.data.length}
-                        pageRangeDisplayed={5}
-                        onChange={this.handlePageChange}
-                    />
-                </div>
+                <Pagination onClick={this.handlePageChange}>
+                    {this.state.showFirst ? (<Pagination.First />) : null}
+                    {this.state.showPrev ? (<Pagination.Prev />) : null}
+                    {this.generatePageItems()}
+                    {this.state.showNext ? (<Pagination.Next />) : null}
+                    {this.state.showLast ? (<Pagination.Last />) : null}
+                </Pagination>
             </div>
         );
     }
