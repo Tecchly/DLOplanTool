7d13c2f8d21cd11fcd8eeeb7166bd17e0485cb53
yrin453 <54128734+yrin453@users.noreply.github.com>
2019-09-19 18:54:01 +1200

    Feedback info displayed but needs proper styling
    In right location this time

--- src/Feedback.js
+++ src/Feedback.js
@@ -1,148 +1,469 @@
 import React from "react";
 import { app } from "./Firebase";
-import { makeStyles } from "@material-ui/core/styles";
+import firestore from "firebase/firestore";
+import Firestore from "./Firestore.js";
 import { Button, Icon } from "antd";
 import { Container, Navbar, Nav, Row, Col, Image } from "react-bootstrap";
 import history from "./history";
 import Ionicon from "react-ionicons";
 import "./index.css";
 
+
+function beef() {
+  console.log(app.auth().currentUser.uid);
+}
+
+
+function isEmpty(obj) {
+  for (var key in obj) {
+    if (obj[key].projectID) 
+      return false;
+  }
+  return true;
+}
+
+
 const Feedback = ({ history }) => {
-  return (
-    <React.Fragment>
-      <Navbar
-        collapseOnSelect
-        expand="lg"
-        bg="light"
-        variant="light"
-        style={{
-          boxShadow: "0px 2px 10px -4px rgba(0,0,0,0.2)"
-        }}
-      >
-        <Container
-          fluid
+  
+  if (!isEmpty(feedbackData)) {
+
+    console.log("you have feedback");
+
+    return (
+      <React.Fragment>
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          bg="light"
+          variant="light"
           style={{
-            alignSelf: "center",
-            alignContent: "center",
-            justifyContent: "center"
+            boxShadow: "0px 2px 10px -4px rgba(0,0,0,0.2)"
           }}
         >
-          <Col />
-          <Col
-            className="justify-content-md-center"
-            xs={11}
-            style={{ textAlign: "center" }}
+          <Container
+            fluid
+            style={{
+              alignSelf: "center",
+              alignContent: "center",
+              justifyContent: "center"
+            }}
           >
-            <Navbar.Brand
-              style={{
-                textAlign: "center",
-                color: "#FA8231",
-                fontFamily: "Montserrat",
-                fontWeight: "600",
-                fontSize: 22
-              }}
-              href="#"
+            <Col />
+            <Col
+              className="justify-content-md-center"
+              xs={11}
+              style={{ textAlign: "center" }}
             >
-              <Image
-                src={require("../assets/images/orange_logop.png")}
-                style={{ height: 30, marginLeft: 5, marginBottom: 2 }}
-              />
-              Digital Learning
-            </Navbar.Brand>
-          </Col>
-          <Col style={{}}>
-            <Nav className="mr-auto"></Nav>
-            <Nav>
-              <img
-                alt="profile"
-                src={app.auth().currentUser.photoURL}
+              <Navbar.Brand
                 style={{
-                  width: 40,
-                  height: 40,
-                  borderRadius: 20,
+                  textAlign: "center",
+                  color: "#FA8231",
+                  fontFamily: "Montserrat",
+                  fontWeight: "600",
+                  fontSize: 22
+                }}
+                href="#"
+              >
+                <Image
+                  src={require("../assets/images/orange_logop.png")}
+                  style={{ height: 30, marginLeft: 5, marginBottom: 2 }}
+                />
+                Digital Learning
+              </Navbar.Brand>
+            </Col>
+            <Col style={{}}>
+              <Nav className="mr-auto"></Nav>
+              <Nav>
+                <img
+                  alt="profile"
+                  src={app.auth().currentUser.photoURL}
+                  style={{
+                    width: 40,
+                    height: 40,
+                    borderRadius: 20,
+                    cursor: "pointer"
+                  }}
+                  onClick={() => {
+                    localStorage.setItem("user", null);
+                    app.auth().signOut();
+                    history.push("/login");
+                  }}
+                />
+              </Nav>
+            </Col>
+          </Container>
+        </Navbar>
+        <Container fluid={true}>
+          <Container
+            style={{ marginTop: 40, marginLeft: 100, marginRight: 100 }}
+            fluid
+          >
+            <Row>
+              <Icon
+                type="arrow-left"
+                onClick={() => history.push("/")}
+                style={{
+                  fontSize: 30,
+                  marginRight: 10,
+                  color: "#2F4858",
                   cursor: "pointer"
                 }}
-                onClick={() => {
-                  localStorage.setItem("user", null);
-                  app.auth().signOut();
-                  history.push("/login");
+              />
+              <h3
+                style={{
+                  color: "#2F4858",
+                  fontFamily: "Montserrat",
+                  fontWeight: "700"
+                }}
+              >
+                Feedback
+              </h3>
+            </Row>
+          </Container>
+            <Container
+              class="d-flex align-items-center"
+              style={{ marginTop: "20vh" }}
+            >
+              <Row></Row>
+                {renderFeedbackData()}
+            </Container>
+        </Container>
+      </React.Fragment>
+    );
+
+  }
+  else {
+    console.log("no feedback");
+    return (
+      <React.Fragment>
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          bg="light"
+          variant="light"
+          style={{
+            boxShadow: "0px 2px 10px -4px rgba(0,0,0,0.2)"
+          }}
+        >
+          <Container
+            fluid
+            style={{
+              alignSelf: "center",
+              alignContent: "center",
+              justifyContent: "center"
+            }}
+          >
+            <Col />
+            <Col
+              className="justify-content-md-center"
+              xs={11}
+              style={{ textAlign: "center" }}
+            >
+              <Navbar.Brand
+                style={{
+                  textAlign: "center",
+                  color: "#FA8231",
+                  fontFamily: "Montserrat",
+                  fontWeight: "600",
+                  fontSize: 22
+                }}
+                href="#"
+              >
+                <Image
+                  src={require("../assets/images/orange_logop.png")}
+                  style={{ height: 30, marginLeft: 5, marginBottom: 2 }}
+                />
+                Digital Learning
+              </Navbar.Brand>
+            </Col>
+            <Col style={{}}>
+              <Nav className="mr-auto"></Nav>
+              <Nav>
+                <img
+                  alt="profile"
+                  src={app.auth().currentUser.photoURL}
+                  style={{
+                    width: 40,
+                    height: 40,
+                    borderRadius: 20,
+                    cursor: "pointer"
+                  }}
+                  onClick={() => {
+                    localStorage.setItem("user", null);
+                    app.auth().signOut();
+                    history.push("/login");
+                  }}
+                />
+              </Nav>
+            </Col>
+          </Container>
+        </Navbar>
+        <Container fluid={true}>
+          <Container
+            style={{ marginTop: 40, marginLeft: 100, marginRight: 100 }}
+            fluid
+          >
+            <Row>
+              <Icon
+                type="arrow-left"
+                onClick={() => history.push("/")}
+                style={{
+                  fontSize: 30,
+                  marginRight: 10,
+                  color: "#2F4858",
+                  cursor: "pointer"
                 }}
               />
-            </Nav>
-          </Col>
-        </Container>
-      </Navbar>
-      <Container fluid={true}>
-        <Container
-          style={{ marginTop: 40, marginLeft: 100, marginRight: 100 }}
-          fluid
-        >
-          <Row>
-            <Icon
-              type="arrow-left"
-              onClick={() => history.push("/")}
-              style={{
-                fontSize: 30,
-                marginRight: 10,
-                color: "#2F4858",
-                cursor: "pointer"
-              }}
-            />
-            <h3
-              style={{
-                color: "#2F4858",
-                fontFamily: "Montserrat",
-                fontWeight: "700"
-              }}
+              <h3
+                style={{
+                  color: "#2F4858",
+                  fontFamily: "Montserrat",
+                  fontWeight: "700"
+                }}
+              >
+                Feedback
+              </h3>
+            </Row>
+          </Container>
+          
+            <Container
+              class="d-flex align-items-center"
+              style={{ marginTop: "20vh" }}
             >
-              Feedback
-            </h3>
-          </Row>
+              <Row></Row>
+              <Row className="justify-content-md-center">
+                <Image
+                  src={require("../assets/images/feedback.svg")}
+                  style={{ height: 220 }}
+                />
+              </Row>
+              <Row className="justify-content-md-center">
+                <h1
+                  style={{
+                    textAlign: "center",
+                    color: "#3A4A56",
+                    fontFamily: "Montserrat",
+                    fontWeight: "700",
+                    textAlign: "center",
+                    fontSize: 30
+                  }}
+                >
+                  No Feedback
+                </h1>
+              </Row>
+              <Row className="justify-content-md-center">
+                <h2
+                  style={{
+                    textAlign: "center",
+                    color: "#8fa5b5",
+                    fontFamily: "Montserrat",
+                    fontWeight: "600",
+                    textAlign: "center",
+                    fontSize: 15
+                  }}
+                >
+                  When feedback is given, you will see it here!
+                </h2>
+              </Row>
+            </Container>
+          
         </Container>
-        <Container
-          class="d-flex align-items-center"
-          style={{ marginTop: "20vh" }}
-        >
-          <Row></Row>
-          <Row className="justify-content-md-center">
-            <Image
-              src={require("../assets/images/feedback.svg")}
-              style={{ height: 220 }}
-            />
-          </Row>
-          <Row className="justify-content-md-center">
-            <h1
-              style={{
-                textAlign: "center",
-                color: "#3A4A56",
-                fontFamily: "Montserrat",
-                fontWeight: "700",
-                textAlign: "center",
-                fontSize: 30
-              }}
-            >
-              No Feedback
-            </h1>
-          </Row>
-          <Row className="justify-content-md-center">
-            <h2
-              style={{
-                textAlign: "center",
-                color: "#8fa5b5",
-                fontFamily: "Montserrat",
-                fontWeight: "600",
-                textAlign: "center",
-                fontSize: 15
-              }}
-            >
-              When feedback is given, you will see it here!
-            </h2>
-          </Row>
-        </Container>
-      
-      </Container>
-    </React.Fragment>
-  );
-};
+      </React.Fragment>
+    );
+  }
+}
+
+function renderFeedbackData() {
+  var feedback;
+
+  feedbackData.forEach(item => {
+    console.log(app.firestore().collection("users").doc(item.reviewerID).projects);
+    feedback += app.firestore().collection("users").doc(item.reviewerID).Name + " left feedback on your project " + item.projectID;
+    feedback += "";
+    feedback += item.type == "praise" ?  " COMMEND IMG": "";
+    feedback += item.comment;
+
+  });
+
+  return feedback;
+}
+
+// const displayFeedback = ({ history }) => {
+
+//   beef();
+//   console.log(isEmpty(feedbackData));
+
+// return (
+//     <React.Fragment>
+//       <Navbar
+//         collapseOnSelect
+//         expand="lg"
+//         bg="light"
+//         variant="light"
+//         style={{
+//           boxShadow: "0px 2px 10px -4px rgba(0,0,0,0.2)"
+//         }}
+//       >
+//         <Container
+//           fluid
+//           style={{
+//             alignSelf: "center",
+//             alignContent: "center",
+//             justifyContent: "center"
+//           }}
+//         >
+//           <Col />
+//           <Col
+//             className="justify-content-md-center"
+//             xs={11}
+//             style={{ textAlign: "center" }}
+//           >
+//             <Navbar.Brand
+//               style={{
+//                 textAlign: "center",
+//                 color: "#FA8231",
+//                 fontFamily: "Montserrat",
+//                 fontWeight: "600",
+//                 fontSize: 22
+//               }}
+//               href="#"
+//             >
+//               <Image
+//                 src={require("../assets/images/orange_logop.png")}
+//                 style={{ height: 30, marginLeft: 5, marginBottom: 2 }}
+//               />
+//               Digital Learning
+//             </Navbar.Brand>
+//           </Col>
+//           <Col style={{}}>
+//             <Nav className="mr-auto"></Nav>
+//             <Nav>
+//               <img
+//                 alt="profile"
+//                 src={app.auth().currentUser.photoURL}
+//                 style={{
+//                   width: 40,
+//                   height: 40,
+//                   borderRadius: 20,
+//                   cursor: "pointer"
+//                 }}
+//                 onClick={() => {
+//                   localStorage.setItem("user", null);
+//                   app.auth().signOut();
+//                   history.push("/login");
+//                 }}
+//               />
+//             </Nav>
+//           </Col>
+//         </Container>
+//       </Navbar>
+//       <Container fluid={true}>
+//         <Container
+//           style={{ marginTop: 40, marginLeft: 100, marginRight: 100 }}
+//           fluid
+//         >
+//           <Row>
+//             <Icon
+//               type="arrow-left"
+//               onClick={() => history.push("/")}
+//               style={{
+//                 fontSize: 30,
+//                 marginRight: 10,
+//                 color: "#2F4858",
+//                 cursor: "pointer"
+//               }}
+//             />
+//             <h3
+//               style={{
+//                 color: "#2F4858",
+//                 fontFamily: "Montserrat",
+//                 fontWeight: "700"
+//               }}
+//             >
+//               Feedback
+//             </h3>
+//           </Row>
+//         </Container>
+        
+        
+        
+//           <Container
+//             class="d-flex align-items-center"
+//             style={{ marginTop: "20vh" }}
+//           >
+//             <Row></Row>
+//             <Row className="justify-content-md-center">
+//               <Image
+//                 src={require("../assets/images/feedback.svg")}
+//                 style={{ height: 220 }}
+//               />
+//             </Row>
+//             <Row className="justify-content-md-center">
+//               <h1
+//                 style={{
+//                   textAlign: "center",
+//                   color: "#3A4A56",
+//                   fontFamily: "Montserrat",
+//                   fontWeight: "700",
+//                   textAlign: "center",
+//                   fontSize: 30
+//                 }}
+//               >
+//                 No Feedback
+//               </h1>
+//             </Row>
+//             <Row className="justify-content-md-center">
+//               <h2
+//                 style={{
+//                   textAlign: "center",
+//                   color: "#8fa5b5",
+//                   fontFamily: "Montserrat",
+//                   fontWeight: "600",
+//                   textAlign: "center",
+//                   fontSize: 15
+//                 }}
+//               >
+//                 When feedback is given, you will see it here!
+//               </h2>
+//             </Row>
+//           </Container>
+        
+//       </Container>
+//     </React.Fragment>
+//   );
+// };
 
 export default Feedback;
+
+
+// mock test data
+
+var feedbackData1 = [
+    { "projectID": "",
+      "reviewerID": "",
+      "timestamp": "",
+      "type": "",
+      "comment": ""
+    }
+];
+
+var feedbackData = [
+  { "projectID": "7bw8VvWbHKdY3AXxKMPF",
+    "reviewerID": "bn2YITj65beCuLnQmEfVdE6Qfup2",
+    "timestamp": "September 2 2019 13:32",
+    "type": "feedback",
+    "comment": "Well done overall, looking forward to seeing the finished project!"
+  },
+  { "projectID": "7bw8VvWbHKdY3AXxKMPF",
+    "reviewerID": "bn2YITj65beCuLnQmEfVdE6Qfup2",
+    "timestamp": "August 31 2019 12:20" ,
+    "type": "praise",
+    "comment": "Good mapping to mode"
+  },
+  { "projectID": "cABtDFfLjeubLrsHOwbV",
+    "reviewerID": "bn2YITj65beCuLnQmEfVdE6Qfup2",
+    "timestamp": "August 31 2019 12:22",
+    "type": "comment",
+    "comment": "I think you should look into a different methond of expressing the idea."
+  }
+];
